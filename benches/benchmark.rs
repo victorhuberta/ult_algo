0c86605b01d445b658e636e55ecc83484e2b1d3e
Victor Huberta <victorhuberta@gmail.com>
2018-04-17 21:20:11 +0700

    Add benchmark for levenshtein distance.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -5,13 +5,19 @@ extern crate ult_algo;
 use criterion::Criterion;
 use ult_algo::sequence;
 
-fn benchmark(c: &mut Criterion) {
+fn sequence_benchmark(c: &mut Criterion) {
     let sequence_: Vec<i32> = (-100..100).collect();
     let pattern: Vec<i32> = (101..200).collect();
     c.bench_function("sequence::match_::bitap(&sequence, &pattern)", move |b| {
         b.iter(|| sequence::match_::bitap(&sequence_, &pattern))
     });
+
+    let source: Vec<char> = "sitting in my kitchen like a boss knitting its scarf".chars().collect();
+    let target: Vec<char> = "kittens love sitting on the knit of my scarf in my kitchen".chars().collect();
+    c.bench_function("sequence::match_::levenshtein_distance(&source, &target)", move |b| {
+        b.iter(|| sequence::match_::levenshtein_distance(&source, &target))
+    });
 }
 
-criterion_group!(benches, benchmark);
+criterion_group!(benches, sequence_benchmark);
 criterion_main!(benches);
